58b32786baba21555b545ba44a6eeccb75171a07
telugu-boy <42505125+telugu-boy@users.noreply.github.com>
2021-01-04 04:57:11 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -6,7 +6,7 @@ echo "Creating venv..."
 python -m venv venv
 
 echo "Installing packages to venv..."
-venv\Scripts\pip install -q PyYAML==5.3.1 beautifulsoup4 requests imageio matplotlib numpy pandas scikit-image scikit-learn imageio-ffmpeg tqdm
+venv\Scripts\pip install -q PyYAML==5.3.1 beautifulsoup4 requests imageio matplotlib numpy==1.19.3 pandas scikit-image scikit-learn imageio-ffmpeg tqdm
 venv\Scripts\pip install -q --timeout 1000 torch===1.6.0 torchvision===0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
 
 venv\Scripts\python ../generate.py
